73a81547d50feedaf5db5d9b7ccec2d6935d5433
Thais Freitas <tfreitas@thoughtworks.com>
2017-10-17 09:44:38 -0300

    refactor: adapt the tests to the new structures

--- test/web/invoice.test.js
+++ test/web/invoice.test.js
@@ -1,19 +1,28 @@
-const request = require("supertest");
-const express = require("express");
-const app = require("../../src/web");
-
 describe('Routes: Invoices', () => {
+  let request;
+
+  before( ()=> {
+    return setupApp()
+      .then(app => {
+        request = supertest(app)
+      })
+  });
+
   const defaultInvoice = {
     name : 'Posto de Gasolina',
     cnpj : '0987654321',
     price: 'R$ 100,00'
   };
+
   describe('GET /invoices', () => {
     it('should return a list of invoices', done => {
-      request(app)
+      request
       .get('/invoices')
-      .set('Accept', 'application/json')
-      .expect(200, [defaultInvoice], done);
+      .end((err, res) => {
+        console.log(res.body[0]);
+        expect(defaultInvoice);
+        done(err);
+      });
     });
   });
 });
