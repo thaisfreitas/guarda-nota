ee4dbaecde308619b9475021345e6af8d02d648a
Thais Freitas <tfreitas@thoughtworks.com>
2017-10-17 09:43:50 -0300

    refactor: add new global variables

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,8 @@
 const chai = require ('chai');
+const supertest = require ('supertest');
+const setupApp = require('../src/app.js');
+
+global.setupApp = setupApp;
+global.supertest = supertest;
 
 global.expect  = chai.expect;
