f5151b6e64d0db43d9c09b28120758df631696a8
Thais Freitas <tfreitas@thoughtworks.com>
2017-10-17 09:47:10 -0300

    refactor: remove bin/ folder

--- bin/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-const web = require("../src/web");
-
-app.use('/index', web);
-
-app.listen(3000, () => console.log("Up and running!"));
