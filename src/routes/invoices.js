1df1e8c00642847364f70716f337ef3f4b9c6e12
Thais Moura <tfreitas@thoughtworks.com>
2018-01-18 19:24:53 -0300

    feat: integration btw routes, controller and models for get invoice

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import InvoiceController from '../controllers/invoices';
+import Invoice from '../models/invoice';
 
 const router = express.Router();
-const invoicesController = new InvoiceController();
+const invoicesController = new InvoiceController(Invoice);
 
 router.get('/', (req, res) => invoicesController.get(req, res));
 
--- test/integration/routes/invoices_spec.js
+++ test/integration/routes/invoices_spec.js
@@ -1,3 +1,5 @@
+import Invoice from '../../../src/models/invoice';
+
 describe('Routes: Invoices', () => {
 
   let request;
@@ -14,12 +16,35 @@ describe('Routes: Invoices', () => {
     description: 'Invoices description',
     price: 100
   };
+
+  const expectedInvoice = {
+    __v: 0,
+    _id: '56cb91bdc3464f14678934ca',
+    name: 'Default Invoices',
+    description: 'Invoices description',
+    price: 100
+  };
+
+  //setting up for database
+  beforeEach(() => {
+    const invoice = new Invoice(defaultInvoice);
+    invoice._id = '56cb91bdc3464f14678934ca';
+    return Invoice.remove({})
+      .then(() => invoice.save());
+  });
+
+  //clear the database
+  afterEach(() => {
+    Invoice.remove({});
+    Invoice.db.close();
+  });
+
   describe('GET /invoices', () => {
     it('should return a list of invoices', done => {
       request
         .get('/invoices')
         .end((err, res) => {
-          expect(res.body[0]).to.eql(defaultInvoice);
+          expect(res.body).to.eql([expectedInvoice]);
           done(err);
         });
     });
